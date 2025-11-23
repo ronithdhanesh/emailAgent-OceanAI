e2926440ff27898242d421638a2990e504332971
ronithdhanesh <ronithdhanesh@gmail.com>
2025-11-23 11:44:31 +0530

    prompts logic backend

---
+++ backend/backend.py
@@ -0,0 +1,37 @@
+import json
+from fastapi import FastAPI, HTTPException
+
+
+app = FastAPI()
+
+@app.get("/emails")
+def get_emails():
+    with open(r"backend\emails.json","r") as f:
+        inbox = json.load(f)
+    return inbox
+
+@app.get("/emails/{email_id}")
+def get_email(email_id: int):
+    with open(r"backend\emails.json", "r") as f:
+        inbox = json.load(f)
+    email = next((e for e in inbox if e["id"] == email_id), None)
+    if email is None:
+        raise HTTPException(status_code=404, detail="Email not found")
+    return email
+
+
+#PROMPTS -----------------------------------------------------------------
+
+@app.get("/prompts")
+def get_prompts():
+    with open("prompts.json","r") as f:
+        prompts = json.load(f)
+        return prompts
+    
+
+@app.post("/prompts")
+def update_prompts(prompts: dict):
+    with open("prompts.json", "w") as f:
+        json.dump(prompts,f,indent=2)
+
+    return {"status" : "ok"}
--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-import requests
 import streamlit as st
+import requests
+
+st.title("Prompts system test")
+
+emails = requests.get("")
+prompts = requests.get("http://127.0.0.1:8000/prompts").json()
+
+st.write(f"Categorization Prompt: {prompts["categorization"]}")
+st.write(f"Action Prompt: {prompts["action_item"]}")
+st.write(f"Auto Reply Prompt: {prompts["auto_reply"]}")
+
+cat_prompt = st.sidebar.text_area("Categorization Prompt", prompts["categorization"])
+action_prompt = st.sidebar.text_area("Action Prompt", prompts["action_item"])
+auto_reply_prompt = st.sidebar.text_area("Auto Reply Prompt", prompts["auto_reply"])
+
+if st.sidebar.button("Save"):
+    data = {
+        "categorization": cat_prompt,
+        "action_item": action_prompt,
+        "auto_reply": auto_reply_prompt
+    }
+
+    requests.post("http://127.0.0.1:8000/prompts", json=data)
+    st.write("Saved successfully, Refresh to see new Prompts")
+    st.rerun()
 
 
-
-emails = requests.get("http://127.0.0.1:8000/emails/").json()
-
-
-st.title("email inbox")
-
-
-
-print(emails)
